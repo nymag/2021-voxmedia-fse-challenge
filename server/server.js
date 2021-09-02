c7f6e95d978678cd9d7667d735506ed5a09f316e
Daniel P. Shannon <daniel.shannon@voxmedia.com>
2021-09-02 10:39:13 -0400

    Reduce reducing.

--- server/server.js
+++ server/server.js
@@ -17,11 +17,6 @@ app.get("/videos", (req, res) => {
   return db.fetchAll().then((data) => res.send(data))
 })
 
-// Return an object which maps provider names to a list of their videos.
-app.get("/providers", (req, res) => {
-  res.status(404).end()
-})
-
 // Given a video id, return its data. Express exposes the ID in the URL on
 // `req.params.id` (e.g., `/video/CAW1` => `req.params.id = "CAW1"). It's safe
 // to assume the video ID is valid.
