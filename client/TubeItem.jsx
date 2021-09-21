058f4438597305d7f395e186386a6c115c623ddf
Daniel P. Shannon <daniel.shannon@voxmedia.com>
2021-09-21 10:07:40 -0400

    💥 Start HTML challenge.

--- client/TubeItem.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-// Accept a tube item, render some properties
-// Add css class to the tubeItem
-const TubeItem = ({ item }) => {
-  return <div>{item.title}</div>;
-};
-
-export default TubeItem;
