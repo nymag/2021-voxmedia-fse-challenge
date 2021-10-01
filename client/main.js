5d0fc90495cc55b4ce3f6ab380c8ab74897cda9e
Daniel P. Shannon <daniel.shannon@voxmedia.com>
2021-10-01 15:33:45 -0400

    🍱 Add Parcel-friendly image dependencies.

--- client/main.js
+++ client/main.js
@@ -1,3 +1,24 @@
+/**
+ * Parcel will load non-JS assets if we import them via a static string in the
+ * URL constructor. Otherwise, it won't build the assets.
+ *
+ * (Q.v. https://v2.parceljs.org/languages/javascript/#url-dependencies.)
+ */
+const PRODUCT_IMAGES = {
+  "1968-04-first-issue.jpg": new URL(
+    "~/client/assets/images/1968-04-first-issue.jpg",
+    import.meta.url
+  ),
+  "1968-07-jewish-food.jpg": new URL(
+    "~/client/assets/images/1968-07-jewish-food.jpg",
+    import.meta.url
+  ),
+  "1976-05-gossip.jpg": new URL(
+    "~/client/assets/images/1976-05-gossip.jpg",
+    import.meta.url
+  ),
+};
+
 const fetchProduct = async () => {
   const products = await (await fetch("http://localhost:8080/products")).json();
   const rand = Math.floor(Math.random() * 3);
@@ -11,9 +32,10 @@ const showProduct = (product, selector) => {
 
   const hed = el.querySelector("h3");
   const img = el.querySelector("img");
+  const imageUrl = PRODUCT_IMAGES[product.product.imageUrl];
 
   if (hed) hed.textContent = product.product.name;
-  if (img) img.src = product.product.imageUrl;
+  if (img) img.src = imageUrl;
 };
 
 (async () => {
